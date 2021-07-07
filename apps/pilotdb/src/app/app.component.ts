cd7b8cf328b79eececcaae8e6ff3cc252fb34872
Patrick Michalina <patrickmichalina@mac.com>
2021-07-07 00:47:16 -0500

    initial commit

---
+++ apps/pilotdb/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pilotwings-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'pilotdb';
+}
